f3555706ea8656f4ba577a2bf12a6d8adb58acb7
BobanKankaras <silvanus930@outlook.com>
2023-04-08 05:29:37 +0900

    update: db url

--- config/initializers/slack_ruby_bot_client.rb
+++ config/initializers/slack_ruby_bot_client.rb
@@ -5,7 +5,7 @@ Slack.configure do |config|
     config.token = "xoxe.xoxb-1-MS0yLTQyNjc3NzE1NDYzMzctNTA2OTQyMDMwOTYzNS01MDc5MjM4MjY2ODgyLTUwODE3MTE2NTAyOTItMjUyYzJiMjhjYjVjM2QwNTlhZmQ3ZjRhNTA4MzBlMGQ5ODljMTZmODY5MmE5ODk3MTAyMjIyZmY2NjU0NGI2Yg"
 
     # for local testing
-    # config.token = "xoxb-4267771546337-5079291343138-eWCVJbzikXPzljR6t3zilRxz"
+    config.token = "xoxb-4267771546337-5079291343138-eWCVJbzikXPzljR6t3zilRxz"
     
     # config.token ||= ENV['SLACK_BOT_TOKEN']
 end
--- config/initializers/slack_ruby_bot_server.rb
+++ config/initializers/slack_ruby_bot_server.rb
@@ -2,7 +2,7 @@
 
 SlackRubyBotServer.configure do |config|
   config.oauth_version = :v2
-  config.oauth_scope = ['users:read', 'channels:read', 'groups:read', 'chat:write', 'commands', 'incoming-webhook']
+  config.oauth_scope = ['users:read', 'channels:read', 'groups:read', 'chat:write', 'commands', 'incoming-webhook', 'channels:manage']
 end
 
 
