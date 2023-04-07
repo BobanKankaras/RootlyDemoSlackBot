c4155f9fa0395a7308fb47a3f374795dc8b9b0f6
BobanKankaras <silvanus930@outlook.com>
2023-04-07 19:16:12 +0900

    update config.hosts

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,7 @@ require_relative "application"
 
 Rails.application.configure do
     # config.hosts << /[a-z0-9-.]+\.ngrok\.io/
+    config.hosts << rootly-demo-incident.onrender.com
     # puts config.hosts
   end
 
