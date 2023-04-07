5a86cd3d7c320b51d2bf26d358907b8dd6682c8a
BobanKankaras <silvanus930@outlook.com>
2023-04-07 19:50:47 +0900

    update: config.token

--- config/initializers/slack_ruby_bot_client.rb
+++ config/initializers/slack_ruby_bot_client.rb
@@ -1,7 +1,7 @@
 require 'slack-ruby-client'
 
 Slack.configure do |config|
-    config.token = "xoxb-4267771546337-5069420309635-ixzJlECscHwlmLsbSxDV4Idb"
+    config.token = "xoxe.xoxb-1-MS0yLTQyNjc3NzE1NDYzMzctNTA2OTQyMDMwOTYzNS01MDcwODc0OTkwODM4LTUwNjI5NDA2NDc0OTUtYjE2MmRiMzM5NTU4YmQ0NzBkYTIyNzgwN2U2NDAyY2I0YjljMWZmMGViNmU4YTIwOGJkOWI5MDAxMzRlY2NhYg"
     # config.token ||= ENV['SLACK_BOT_TOKEN']
     # puts 'Token: ' + config.token
 end
